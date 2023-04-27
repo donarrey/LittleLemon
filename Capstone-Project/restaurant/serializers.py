4573dee548633c092f0a6d7e1df4de052c925d69
donarrey <122565939+donarrey@users.noreply.github.com>
2023-04-27 04:19:11 +0200

    Update serializers.py

--- Capstone-Project/restaurant/serializers.py
+++ Capstone-Project/restaurant/serializers.py
@@ -1,12 +1,20 @@
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from .models import MenuItem, BookingTable
 from rest_framework import serializers
-from .models import MenuIterm
 
-class MenuItemSerializer(serializers.ModelSerializers):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
-#class BookingSerializer(serializers.ModelSerializers):
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = "__all__"
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookingTable
+        fields = "__all__"
     
     
